239dee7840d22d306826fb7bf93f25eda521a955
Martin <102524041+martinl5@users.noreply.github.com>
2026-03-31 13:48:43 +0800

    Feature: Add Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en" className="h-full antialiased">
       <body className="h-full overflow-hidden" suppressHydrationWarning>
         <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
